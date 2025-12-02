a619817a34ee83bf8bef83e46db093051b40a296
W <localgit@w-wins.com>
2025-12-02 11:27:53 +0100

    Simplify main

--- TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayTwoMain.java
+++ TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayTwoMain.java
@@ -1,20 +1,19 @@
 package com.w_wins.advent.twentytwentyfive.mains;
 
 import com.w_wins.advent.twentytwentyfive.daytwo.Range;
-import com.w_wins.adventcommon.AdventTwoPartEvaluator;
 import com.w_wins.iostream.Utf8ResourceLines;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.function.Function;
 
 public class DayTwoMain {
     static void main() {
-        IO.println(new Utf8ResourceLines(Range.class, "/input.txt").evaluate(new AdventTwoPartEvaluator(lines->{
-            final List<Range> ranges = Range.parse(lines);
-            return ranges.stream().map(Range::sumDoubles).reduce(BigInteger.ZERO,BigInteger::add);
-        }, lines -> {
-            final List<Range> ranges = Range.parse(lines);
-            return ranges.stream().map(Range::sumMultiples).reduce(BigInteger.ZERO,BigInteger::add);
-        })));
+        final List<Range> ranges = new Utf8ResourceLines(Range.class, "/input.txt").evaluate(Range::parse);
+        IO.println("Part 1: "+ sum(ranges, Range::sumDoubles) +", part 2: "+ sum(ranges, Range::sumMultiples));
+    }
+
+    private static BigInteger sum(final List<Range> ranges, final Function<Range, BigInteger> method) {
+        return ranges.stream().map(method).reduce(BigInteger.ZERO, BigInteger::add);
     }
 }
