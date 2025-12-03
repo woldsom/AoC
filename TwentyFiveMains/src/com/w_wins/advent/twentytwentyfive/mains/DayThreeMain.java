8dd1228697c448f4cef455224b119b94bf36d492
W <localgit@w-wins.com>
2025-12-03 06:16:56 +0100

    Part 1 solution before rewriting for part 2

--- TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayThreeMain.java
+++ TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayThreeMain.java
@@ -3,11 +3,39 @@ package com.w_wins.advent.twentytwentyfive.mains;
 import com.w_wins.advent.twentytwentyfive.daythree.Three;
 import com.w_wins.iostream.Utf8ResourceLines;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class DayThreeMain {
+
+    public static final Comparator<Integer> DESCENDING = Comparator.<Integer>naturalOrder().reversed();
+
     static void main() {
-        final List<Three> input = new Utf8ResourceLines(Three.class, "/test.txt").evaluate(Three::parse);
-        IO.println(input);
+        final List<List<Integer>> input = new Utf8ResourceLines(Three.class, "/input.txt").evaluate(lines -> lines.map(line -> line.chars().mapToObj(c -> c - '0').toList()).toList());
+        final int sum = input.stream().mapToInt(bank -> {
+            final List<Integer> sorted = getSorted(bank);
+            if (sorted.getFirst().equals(sorted.get(1))) {
+                return sorted.getFirst() * 11;
+            }
+            final int largestPosition = bank.indexOf(sorted.getFirst());
+            if (largestPosition < bank.size() - 1) {
+                return sorted.getFirst() * 10 + getSorted(bank.subList(largestPosition + 1, bank.size())).getFirst();
+            }
+            final Set<Integer> sortedSet = new TreeSet<>(DESCENDING);
+            sortedSet.addAll(sorted);
+            final int secondLargest = sortedSet.stream().skip(1).findFirst().orElseThrow();
+            final int secondLargestPosition = bank.indexOf(secondLargest);
+            return secondLargest * 10 + getSorted(bank.subList(secondLargestPosition + 1, bank.size())).getFirst();
+        }).sum();
+        IO.println(sum);
+    }
+
+    private static List<Integer> getSorted(final List<Integer> unmodifiable) {
+        final List<Integer> sorted = new ArrayList<>(unmodifiable);
+        sorted.sort(DESCENDING);
+        return sorted;
     }
 }
