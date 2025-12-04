3dcfe25aabc3d9b8d5b2e1c933e7ddb1307b7095
W <localgit@w-wins.com>
2025-12-04 05:46:03 +0100

    Microbenchmark

--- TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayThreeMain.java
+++ TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayThreeMain.java
@@ -7,11 +7,18 @@ import com.w_wins.common.Strings;
 import com.w_wins.iostream.Utf8ResourceLines;
 
 import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class DayThreeMain {
     static void main() {
-        IO.println(new Utf8ResourceLines(BatteryBankGatherer.class, "/input.txt").evaluate(new AdventTwoPartEvaluator(run(2), run(12))));
+        IntStream.range(0, 10000).forEach(_ -> {
+            final long nano = System.nanoTime();
+            final String result = new Utf8ResourceLines(BatteryBankGatherer.class, "/input.txt").evaluate(new AdventTwoPartEvaluator(run(2), run(12)));
+            final long time = System.nanoTime() - nano;
+            IO.println(time);
+            IO.println(result);
+        });
     }
 
     private static Function<Stream<String>, Long> run(final int batteryCount) {
