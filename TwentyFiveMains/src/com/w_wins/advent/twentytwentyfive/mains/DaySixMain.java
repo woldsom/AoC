2f28b1c72cf19068d2f8e91b3eef369f4814cfc2
W <localgit@w-wins.com>
2025-12-06 16:54:54 +0100

    Better implementation

--- TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DaySixMain.java
+++ TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DaySixMain.java
@@ -2,80 +2,34 @@ package com.w_wins.advent.twentytwentyfive.mains;
 
 import com.w_wins.advent.twentytwentyfive.daysix.Six;
 import com.w_wins.adventcommon.AdventTwoPartEvaluator;
-import com.w_wins.common.NumberBuilder;
-import com.w_wins.fixedwidth.Grid;
-import com.w_wins.fixedwidth.GridConfig;
-import com.w_wins.fixedwidth.SimpleGridParser;
+import com.w_wins.common.CollectionUtil;
+import com.w_wins.common.Gatherers;
+import com.w_wins.common.Streams;
+import com.w_wins.common.Strings;
 import com.w_wins.iostream.Utf8ResourceLines;
+import com.w_wins.symbolic.Symbol;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.LongBinaryOperator;
-import java.util.stream.IntStream;
-
-import static java.util.function.Predicate.not;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DaySixMain {
     static void main() {
-        IO.println(new Utf8ResourceLines(Six.class, "/input.txt").evaluate(new AdventTwoPartEvaluator(lines -> {
-            final List<List<String>> input = lines.map(line -> Arrays.stream(line.split("\\s+")).filter(not(String::isBlank)).toList()).toList();
-            final List<List<Integer>> numbers = new ArrayList<>();
-            final List<String> operators = new ArrayList<>();
-            input.forEach(row -> {
-                final List<Integer> rowList = new ArrayList<>();
-                row.forEach(column -> {
-                    try {
-                        rowList.add(Integer.parseInt(column.trim()));
-                    } catch (NumberFormatException _) {
-                        operators.add(column.trim());
-                    }
-                });
-                if (!rowList.isEmpty()) {
-                    numbers.add(rowList);
-                }
-            });
-            return IntStream.range(0, operators.size()).mapToLong(index -> {
-                final LongBinaryOperator function = switch (operators.get(index)) {
-                    case "*" -> Math::multiplyExact;
-                    case "+" -> Math::addExact;
-                    default -> throw new IllegalArgumentException(operators.get(index));
-                };
-                return numbers.stream().mapToLong(number -> number.get(index)).reduce(function).orElseThrow();
-            }).sum();
-        }, lines -> {
-            final List<String> list = new ArrayList<>(lines.toList());
-            final int longest = list.stream().mapToInt(String::length).max().orElseThrow();
-            list.replaceAll(line -> line.concat(" ".repeat(longest - line.length())));
-            final Grid<String> grid = new SimpleGridParser(GridConfig.CHAR_GRID).apply(list.stream());
-            LongBinaryOperator function = null;
-            final List<Long> numbers = new ArrayList<>();
-            long sum = 0;
-            for (int column = 0; column < grid.getColumnCount(); ++column) {
-                final NumberBuilder builder = new NumberBuilder();
-                boolean spaces = true;
-                for (int row = 0; row < grid.getRowCount(); ++row) {
-                    final char character = grid.get(column, row).charAt(0);
-                    if (character != ' ') {
-                        spaces = false;
-                        if (row == grid.getRowCount() - 1) {
-                            function = switch (character) {
-                                case '*' -> Math::multiplyExact;
-                                case '+' -> Math::addExact;
-                                default -> throw new IllegalArgumentException("" + character);
-                            };
-                        } else {
-                            builder.addDigit(character);
-                        }
-                    }
-                }
-                builder.getAndZero().stream().mapToLong(x -> x).forEach(numbers::add);
-                if (spaces || column == grid.getColumnCount() - 1) {
-                    sum = Math.addExact(sum, numbers.stream().mapToLong(x -> x).reduce(function).orElseThrow());
-                    numbers.clear();
-                }
-            }
-            return sum;
-        })));
+        IO.println(new Utf8ResourceLines(Six.class, "/input.txt").evaluate(new AdventTwoPartEvaluator(DaySixMain::partOne, DaySixMain::partTwo)));
+    }
+
+    private static long partOne(Stream<String> lines) {
+        final List<LongBinaryOperator> operators = new ArrayList<>();
+        final List<List<Long>> operands = CollectionUtil.transpose(Streams.splitLast(lines, line -> Strings.split(line).map(Symbol::asLongOperator).collect(Collectors.toCollection(()->operators))).map(line -> Strings.split(line).map(Long::parseLong).toList()).toList());
+        return CollectionUtil.zip(operators, operands,(operator,localOperands)->localOperands.stream().reduce(operator::applyAsLong).orElseThrow()).stream().reduce(Math::addExact).orElseThrow();
+    }
+
+    private static long partTwo(Stream<String> lines) {
+        return CollectionUtil.transpose(lines.map(line -> Strings.characters(line).toList()).toList()).stream().gather(Gatherers.splitWhen(list -> list.stream().allMatch(Character::isWhitespace))).mapToLong(grid -> {
+            final LongBinaryOperator operator = Symbol.asLongOperator(grid.getFirst().getLast().toString());
+            return grid.stream().mapToLong(line -> Long.parseLong(line.stream().filter(Character::isDigit).map(Object::toString).collect(Collectors.joining()).trim())).reduce(operator).orElseThrow();
+        }).sum();
     }
 }
