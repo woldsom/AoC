56aa03a7a4c78f0f8264ed2739045d09911db2a1
W <localgit@w-wins.com>
2025-12-07 06:19:34 +0100

    Actual day 7 implementations

--- TwentyFiveDaySeven/src/com/w_wins/advent/twentytwentyfive/dayseven/Seven.java
+++ TwentyFiveDaySeven/src/com/w_wins/advent/twentytwentyfive/dayseven/Seven.java
@@ -1,22 +1,14 @@
 package com.w_wins.advent.twentytwentyfive.dayseven;
 
 public enum Seven {
-    WALL, FLOOR, PILL;
+    SPLITTER, FLOOR, BEGIN;
 
     public static Seven parse(String s) {
         return switch (s) {
-            case "#" -> WALL;
+            case "^" -> SPLITTER;
             case "." -> FLOOR;
-            case "*" -> PILL;
+            case "S" -> BEGIN;
             default -> throw new IllegalArgumentException("Unknown grid string " + s);
-        };
-    }
-
-    public String symbol() {
-        return switch(this){
-            case WALL->"X";
-            case FLOOR -> "_";
-            case PILL -> "O";
         };
     }
 }
--- TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DaySevenMain.java
+++ TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DaySevenMain.java
@@ -9,31 +9,35 @@ import com.w_wins.fixedwidth.Grid;
 import com.w_wins.fixedwidth.GridConfig;
 import com.w_wins.fixedwidth.SimpleGridParser;
 import com.w_wins.iostream.Utf8ResourceLines;
-import com.w_wins.pathfinding.ScoredPath;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static com.w_wins.advent.twentytwentyfive.dayseven.Seven.FLOOR;
-import static com.w_wins.advent.twentytwentyfive.dayseven.Seven.PILL;
-import static com.w_wins.advent.twentytwentyfive.dayseven.Seven.WALL;
+import static com.w_wins.advent.twentytwentyfive.dayseven.Seven.BEGIN;
+import static com.w_wins.advent.twentytwentyfive.dayseven.Seven.SPLITTER;
 
 public class DaySevenMain {
     static void main() {
-        final Grid<Seven> grid = new Utf8ResourceLines(Seven.class, "/test.txt").evaluate(lines -> Grids.map(new SimpleGridParser(GridConfig.CHAR_GRID).apply(lines), Functions.extraLeft(Seven::parse)));
-        Grids.debug(grid, x -> x.name().substring(0, 1));
-        final List<Coordinate> pills = new ArrayList<>(Grids.findAll(grid, PILL));
-        final ScoredPath<Integer, Coordinate> longestPath = CollectionUtil.selfCross(pills, (a, b) -> Grids.shortestPath(grid, a, b, WALL::equals).orElse(null)).filter(Objects::nonNull).max(Comparator.comparing(ScoredPath::cost)).orElseThrow();
-        final List<Coordinate> pathCoordinates = longestPath.path();
-        Grids.debug(Grids.map(grid,(coordinate,object)->{
-            final int index = pathCoordinates.indexOf(coordinate);
-            if(FLOOR==object && index>=0){
-                return ""+index%10;
-            } else {
-                return object.symbol();
-            }
-        }),x->x);
+        final Grid<Seven> grid = new Utf8ResourceLines(Seven.class, "/input.txt").evaluate(lines -> Grids.map(new SimpleGridParser(GridConfig.CHAR_GRID).apply(lines), Functions.extraLeft(Seven::parse)));
+        final SortedSet<Coordinate> splitters = new TreeSet<>(Grids.findAll(grid, SPLITTER));
+        final Coordinate begin = Grids.find(grid, BEGIN);
+        final Map<Integer,Long> beams = new HashMap<>(Map.of(begin.x(),1L));
+        final int totalSplits = IntStream.range(begin.y(), grid.getRowCount()).map(y -> {
+            final Set<Integer> lineSplitters = splitters.tailSet(new Coordinate(0, y)).headSet(new Coordinate(-1, y + 1)).stream().map(Coordinate::x).collect(Collectors.toSet());
+            final Set<Integer> splits = CollectionUtil.intersection(lineSplitters, beams.keySet());
+            splits.stream().forEach(x-> {
+                beams.merge(x + 1, beams.get(x), Math::addExact);
+                beams.merge(x - 1, beams.get(x), Math::addExact);
+            });
+            beams.keySet().removeAll(splits);
+            return splits.size();
+        }).sum();
+        IO.println(totalSplits);
+        IO.println(beams.values().stream().mapToLong(x->x).sum());
     }
 }
