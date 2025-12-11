6e703786e66a5dddc7f02dd95ddf5b2698385181
W <localgit@w-wins.com>
2025-12-11 13:08:49 +0100

    Removed dumb parts now that smart is working

--- TwentyFiveDayEleven/src/com/w_wins/advent/twentytwentyfive/dayeleven/Device.java
+++ TwentyFiveDayEleven/src/com/w_wins/advent/twentytwentyfive/dayeleven/Device.java
@@ -6,7 +6,6 @@ import com.w_wins.common.Strings;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.OptionalLong;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,7 +14,7 @@ public record Device(String label, Set<Device> outputs) {
         return new Device(name, new HashSet<>());
     }
 
-    public static Map.Entry<String, Set<String>> parse(String line) {
+    public static Map.Entry<String, Set<String>> parse(final String line) {
         final List<String> parts = Strings.split(line, ": ").toList();
         return Map.entry(parts.getFirst(), Strings.split(parts.getLast()).collect(Collectors.toSet()));
     }
@@ -45,27 +44,5 @@ public record Device(String label, Set<Device> outputs) {
     @Override
     public int hashCode() {
         return label.hashCode();
-    }
-
-    public OptionalLong pathsToMax(final String target, final long limit, final int depth) {
-        if (target.equals(label())) {
-            return OptionalLong.of(1L);
-        }
-        if (depth == 0) {
-            return OptionalLong.empty();
-        }
-        long sum = 0;
-        for (final Device output : outputs()) {
-            final OptionalLong intermediary = output.pathsToMax(target, limit - sum, depth - 1);
-            if (intermediary.isPresent()) {
-                sum += intermediary.getAsLong();
-                if (sum > limit) {
-                    return OptionalLong.empty();
-                }
-            } else {
-                return OptionalLong.empty();
-            }
-        }
-        return OptionalLong.of(sum);
     }
 }
--- TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayElevenMain.java
+++ TwentyFiveMains/src/com/w_wins/advent/twentytwentyfive/mains/DayElevenMain.java
@@ -2,27 +2,21 @@ package com.w_wins.advent.twentytwentyfive.mains;
 
 import com.w_wins.advent.twentytwentyfive.dayeleven.Device;
 import com.w_wins.advent.twentytwentyfive.dayeleven.Paths;
-import com.w_wins.advent.twentytwentyfive.dayeleven.Shortcut;
 import com.w_wins.collections.BinaryHeap;
-import com.w_wins.common.CollectionUtil;
 import com.w_wins.common.CollectorUtil;
 import com.w_wins.common.Functions;
 import com.w_wins.common.Graphs;
 import com.w_wins.common.Streams;
 import com.w_wins.iostream.Utf8ResourceLines;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.OptionalLong;
-import java.util.Random;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class DayElevenMain {
     static void main() {
@@ -64,63 +58,9 @@ public class DayElevenMain {
                         case "dac" -> paths.butDac();
                         case "fft" -> paths.butFft();
                         default -> paths;
-                    }
-                    , Paths::add));
+                    },
+                    Paths::add));
         }
         IO.println("Part 2: " + graph.get(devices.get("out")).both());
-    }
-
-    private static void dumb(final Map<String, Device> devices) {
-        final List<Device> devicesList = new ArrayList<>(devices.values());
-        final Set<Shortcut> shortcuts = CollectionUtil.selfCross(devicesList, Shortcut::new).<Shortcut>mapMulti((shortcut, downstream) -> {
-            downstream.accept(shortcut);
-            downstream.accept(shortcut.reversed());
-        }).collect(Collectors.toSet());
-        IO.println(shortcuts.size() + " shortcuts");
-        final Map<Shortcut, OptionalLong> shortcutCosts = shortcuts.stream().map(Functions.entry(s -> s.start().pathsToMax(s.end().label(), 10, 10))).collect(CollectorUtil.toMap());
-        final Map<Device, Map<Device, List<OptionalLong>>> startCost2 = shortcutCosts.entrySet().stream().collect(Collectors.groupingBy(e -> e.getKey().start(), Collectors.groupingBy(e -> e.getKey().end(), Collectors.mapping(Map.Entry::getValue, Collectors.toList()))));
-        final Map<Device, Map<Device, OptionalLong>> startCost = startCost2.entrySet().stream().map(Functions.onValue(v -> v.entrySet().stream().map(Functions.onValue(l -> {
-            if (l.size() != 1) {
-                throw new IllegalArgumentException("Wrong list " + l + " for " + v);
-            }
-            return l.getFirst();
-        })).collect(CollectorUtil.toMap()))).collect(CollectorUtil.toMap());
-        final Map<Device, Map<Device, List<OptionalLong>>> endCost2 = shortcutCosts.entrySet().stream().collect(Collectors.groupingBy(e -> e.getKey().end(), Collectors.groupingBy(e -> e.getKey().start(), Collectors.mapping(Map.Entry::getValue, Collectors.toList()))));
-        final Map<Device, Map<Device, OptionalLong>> endCost = endCost2.entrySet().stream().map(Functions.onValue(v -> v.entrySet().stream().map(Functions.onValue(l -> {
-            if (l.size() != 1) {
-                throw new IllegalArgumentException("Wrong list " + l + " for " + v);
-            }
-            return l.getFirst();
-        })).collect(CollectorUtil.toMap()))).collect(CollectorUtil.toMap());
-        IO.println(shortcutCosts.size());
-        final Random random = new Random();
-        while (
-                shortcutCosts.get(new Shortcut(devices.get("you"), devices.get("out"))).isEmpty() &&
-                        shortcutCosts.get(new Shortcut(devices.get("svr"), devices.get("fft"))).isEmpty() &&
-                        shortcutCosts.get(new Shortcut(devices.get("svr"), devices.get("dac"))).isEmpty() &&
-                        shortcutCosts.get(new Shortcut(devices.get("dac"), devices.get("out"))).isEmpty() &&
-                        shortcutCosts.get(new Shortcut(devices.get("fft"), devices.get("out"))).isEmpty()
-
-        ) {
-            final List<Shortcut> emptyList = shortcutCosts.entrySet().stream().filter(e -> e.getValue().isEmpty()).map(Map.Entry::getKey).toList();
-            //final Shortcut target = emptyList.get(random.nextInt(emptyList.size()));
-            final Shortcut target = emptyList.stream().filter(candidate -> startCost.get(candidate.start()).values().stream().allMatch(OptionalLong::isPresent) && endCost.get(candidate.end()).values().stream().allMatch(OptionalLong::isPresent)).findAny().orElseThrow();
-            final Map<Device, OptionalLong> from = startCost.get(target.start());
-            final Map<Device, OptionalLong> to = endCost.get(target.end());
-            if (from.entrySet().stream().filter(e -> !e.getKey().equals(target.end())).map(Map.Entry::getValue).allMatch(OptionalLong::isPresent) && to.entrySet().stream().filter(e -> !e.getKey().equals(target.start())).map(Map.Entry::getValue).allMatch(OptionalLong::isPresent)) {
-                final Set<Device> intersection = CollectionUtil.intersection(from.keySet(), to.keySet());
-                if (intersection.size() != from.size() - 1 || intersection.size() != to.size() - 1) {
-                    throw new IllegalArgumentException("From: " + from.size() + ", to: " + to.size() + ", intersection: " + intersection.size());
-                }
-                final long newCount = from.entrySet().stream().filter(e -> !e.getKey().equals(target.end())).map(Map.Entry::getValue).mapToLong(start ->
-                        Math.multiplyExact(to.entrySet().stream().filter(e -> !e.getKey().equals(target.start())).map(Map.Entry::getValue).mapToLong(OptionalLong::orElseThrow).sum(), start.orElseThrow())
-                ).sum();
-                final OptionalLong value = OptionalLong.of(newCount);
-                shortcutCosts.put(target, value);
-                startCost.get(target.start()).put(target.end(), value);
-                endCost.get(target.end()).put(target.start(), value);
-                IO.println("Remaining: " + emptyList.size());
-            }
-        }
     }
 }
